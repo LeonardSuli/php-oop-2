a2de644a80151fff30702157e59e3c57b4a76cb2
LeonardSuli <leonardsuli1991@gmail.com>
2024-03-23 12:34:08 +0100

    add class product

--- index.php
+++ index.php
@@ -1,8 +1,38 @@
 <?php
 
 // Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
-// L'e-commerce vende prodotti per animali.
-// I prodotti sono categorizzati, le categorie sono Cani o Gatti.
-// I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
-// Stampiamo delle card contenenti i dettagli dei prodotti, 
-// come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).
+
+// 1 - L'e-commerce vende prodotti per animali.
+
+// 2 - I prodotti sono categorizzati, le categorie sono Cani o Gatti.
+
+// 3 - I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+
+// Stampiamo delle card contenenti i dettagli dei prodotti, come:
+// 1 - immagine,
+// 2 - titolo, 
+// 3 - prezzo, 
+// 4 - icona della categoria 
+// 5 - il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).
+
+
+class product
+{
+
+    public $name;
+    public $description;
+    public $image;
+    public $price;
+    public $category;
+    public $articleType;
+
+    public function __construct($name, $description, $image, $price, $category, $articleType)
+    {
+        $this->name = $name;
+        $this->description = $description;
+        $this->image = $image;
+        $this->price = $price;
+        $this->category = $category;
+        $this->articleType = $articleType;
+    }
+}
