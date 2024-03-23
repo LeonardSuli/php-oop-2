bc0e5386233d3531de94fd07ec7b31c1da7f0653
LeonardSuli <leonardsuli1991@gmail.com>
2024-03-23 16:58:47 +0100

    add dog and cat classes cards on page

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ class Product
 }
 
 
-class Dogs extends Product
+class Dog extends Product
 {
     // public $breed;
     // public $size;
@@ -50,12 +50,96 @@ class Dogs extends Product
     }
 }
 
-// $product = new Product('a', 'b', 'c', 'd', 'e');
-$dogs = new Dogs('paperella', 'oggetto di gomma da mordere', 'https://picsum.photos/id/237/400/300', 4.99, 'toys', 'golden retriver', 'medium');
+class Cat extends Product
+{
+    // public $breed;
+    // public $size;
 
-var_dump($dogs);
+    public function __construct($name, $description, $image, $price, $articleType, public $breed, public $size)
+    {
+        parent::__construct($name, $description, $image, $price, $articleType);
+        $this->breed = $breed;
+        $this->size = $size;
+    }
+}
 
 
-// class Cats extends Product
-// {
-// }
+$animals = [
+
+    $dog = new Dog(
+        'Paperella',
+        'Oggetto di gomma da mordere',
+        'https://m.media-amazon.com/images/I/41yvxsHIJ6L._AC_UF1000,1000_QL80_.jpg',
+        '4.99 €',
+        'toys',
+        'Golden Retriver',
+        'Medium'
+    ),
+
+    $cat = new Cat(
+        'Tiragraffi',
+        'Oggetto che mantiene le unghie del gatto sane',
+        'https://m.media-amazon.com/images/I/71AvbGKoyoL._AC_UF894,1000_QL80_.jpg',
+        '29.99 €',
+        'toys',
+        'Siriano',
+        'Small'
+    ),
+
+];
+
+var_dump($dog, $cat);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-oop-1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <main class="mt-5">
+
+        <section>
+            <div class="container">
+                <div class="row row-cols-1 row-cols-sm-2 g-3 text-center">
+
+                    <?php foreach ($animals as $animal) : ?>
+
+                        <div class="col">
+
+                            <div class="card">
+
+                                <div class="card-body">
+                                    <h3><?= $animal->name ?></h3>
+                                    <p><?= $animal->description ?></p>
+                                    <img style="width: 200px;" src="<?= $animal->image ?>" alt="">
+                                    <p><?= $animal->price ?></p>
+                                </div>
+
+                                <div class="card-footer">
+                                    <p><?= $animal->breed ?></p>
+                                    <p><?= $animal->size ?></p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    <?php endforeach; ?>
+
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+</body>
+
+</html>
